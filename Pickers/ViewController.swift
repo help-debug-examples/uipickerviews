780c5a2ea37a7aecb035a4ad56663f5c51b1d878
Derrick Showers <derrick@derrickshowers.com>
2017-02-06 14:37:52 -0800

    Initial commit

--- Pickers/ViewController.swift
+++ Pickers/ViewController.swift
@@ -8,7 +8,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+
+    @IBOutlet weak var fooPickerView: UIPickerView!
+    @IBOutlet weak var barPickerView: UIPickerView!
+
+    let fooPickerViewItems = [
+        "html",
+        "javascript",
+        "css",
+        "swift",
+        "java",
+        "ruby",
+        "python"
+    ]
+
+    let barPickerViewItems = [
+        "steve",
+        "derrick",
+        "jasnö"
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +39,38 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // MARK: - UIPickerViewDelegate
 
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+
+        if pickerView == fooPickerView {
+            return fooPickerViewItems[row]
+        }
+
+        if pickerView == barPickerView {
+            return barPickerViewItems[row]
+        }
+
+        return nil
+    }
+
+    // MARK: - UIPickerViewDataSource
+
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+
+        if pickerView == fooPickerView {
+            return fooPickerViewItems.count
+        }
+
+        if pickerView == barPickerView {
+            return barPickerViewItems.count
+        }
+
+        return 0
+    }
 }
 
